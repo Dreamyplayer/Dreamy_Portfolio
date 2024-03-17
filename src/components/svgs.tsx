82e10154067f5c8a03fc39fac9cbab5ed939ec9e
Dreamyplayer <saikumarchinna123@gmai.com>
2024-03-17 17:06:36 +0530

    Add svg icons

---
+++ src/components/svgs.tsx
@@ -0,0 +1,32 @@
+
+export const System = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'  className='fill-blue-500 h-5 w-5'>
+      <path
+        fillRule='evenodd'
+        d='M2 4.25A2.25 2.25 0 0 1 4.25 2h11.5A2.25 2.25 0 0 1 18 4.25v8.5A2.25 2.25 0 0 1 15.75 15h-3.105a3.501 3.501 0 0 0 1.1 1.677A.75.75 0 0 1 13.26 18H6.74a.75.75 0 0 1-.484-1.323A3.501 3.501 0 0 0 7.355 15H4.25A2.25 2.25 0 0 1 2 12.75v-8.5Zm1.5 0a.75.75 0 0 1 .75-.75h11.5a.75.75 0 0 1 .75.75v7.5a.75.75 0 0 1-.75.75H4.25a.75.75 0 0 1-.75-.75v-7.5Z'
+        clipRule='evenodd'
+      />
+    </svg>
+  );
+};
+
+export const Light = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'  className='fill-yellow-400 h-5 w-5'>
+      <path d='M11.983 1.907a.75.75 0 0 0-1.292-.657l-8.5 9.5A.75.75 0 0 0 2.75 12h6.572l-1.305 6.093a.75.75 0 0 0 1.292.657l8.5-9.5A.75.75 0 0 0 17.25 8h-6.572l1.305-6.093Z' />
+    </svg>
+  );
+};
+
+export const Moon = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'  className='fill-gray-400 h-5 w-5'>
+      <path
+        fillRule='evenodd'
+        d='M7.455 2.004a.75.75 0 0 1 .26.77 7 7 0 0 0 9.958 7.967.75.75 0 0 1 1.067.853A8.5 8.5 0 1 1 6.647 1.921a.75.75 0 0 1 .808.083Z'
+        clipRule='evenodd'
+      />
+    </svg>
+  );
+};
