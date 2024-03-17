e2fa5e35cd130888e86d24d54ad954473ec9bdd3
Dreamyplayer <saikumarchinna123@gmai.com>
2024-03-17 15:52:38 +0530

    Add clean command to scripts, integrate background grids

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+import svgToDataUri from 'mini-svg-data-uri';
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
+
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ':root': newVars,
+  });
+};
 
 const config: Config = {
   content: [
@@ -7,6 +19,26 @@ const config: Config = {
     './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: ['class'],
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          'bg-grid-small': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+          'bg-dot': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' },
+      );
+    },
+  ],
 };
+
 export default config;
