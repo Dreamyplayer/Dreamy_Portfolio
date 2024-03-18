01c18bfa464cfb884c558aafd8b30597c371de70
Dreamyplayer <saikumarchinna123@gmai.com>
2024-03-18 10:54:18 +0530

    🔄 Update page with Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Hero_Section from '@/components/HeroSection';
 import Logo from '@/components/Logo';
+import Footer from '@/ui/Footer';
 
 export default function Home() {
   return (
     <main className='mt-10 h-auto'>
       <Logo />
       <Hero_Section />
+
+      {/* END */}
+      <Footer />
     </main>
   );
 }
