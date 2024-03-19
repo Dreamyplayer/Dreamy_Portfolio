58f4c5fa928383a49457446da27c5a915643df61
Dreamyplayer <saikumarchinna123@gmai.com>
2024-03-19 19:33:58 +0530

    🎨 Change icon color

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -21,7 +21,7 @@ export const Light = () => {
 
 export const Moon = () => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'  className='fill-gray-400 h-5 w-5'>
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'  className='fill-[#808080] dark:fill-[#CCCCCC] h-5 w-5'>
       <path
         fillRule='evenodd'
         d='M7.455 2.004a.75.75 0 0 1 .26.77 7 7 0 0 0 9.958 7.967.75.75 0 0 1 1.067.853A8.5 8.5 0 1 1 6.647 1.921a.75.75 0 0 1 .808.083Z'
