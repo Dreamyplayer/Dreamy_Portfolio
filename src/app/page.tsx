b5b9b3b5d7d2364fec330ead75c2602d6150c426
Dreamyplayer <saikumarchinna123@gmai.com>
2024-03-19 19:30:30 +0530

    ✨ Add sparkles in background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import Hero_Section from '@/components/HeroSection';
+import HeroSection from '@/components/HeroSection';
 import Logo from '@/components/Logo';
 import Footer from '@/ui/Footer';
+import { SparklesCore } from '@/ui/sparkles';
 
 export default function Home() {
   return (
     <main className='mt-10 h-auto'>
+      {/* Core component */}
+      <SparklesCore
+        background='transparent'
+        minSize={0.4}
+        maxSize={1}
+        particleDensity={120}
+        className='fixed -mt-10 h-full w-full'
+      />
+
       <Logo />
-      <Hero_Section />
+      <HeroSection />
 
       {/* END */}
       <Footer />
