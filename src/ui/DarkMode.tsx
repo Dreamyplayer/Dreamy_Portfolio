f8816596446e41e5a2c7af2ecda42bc4cb6482c9
Dreamyplayer <saikumarchinna123@gmai.com>
2024-04-11 00:00:45 +0530

    Upgrade dark mode functionality with Framer Motion animations

--- src/ui/DarkMode.tsx
+++ src/ui/DarkMode.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from 'react';
 
-import IconButton from '@/components/IconButton';
 import { SVGIcons } from '@/components/svgs';
+import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
 
 export type ThemeMode = {
@@ -43,15 +43,30 @@ const DarkModeSwitch = () => {
   };
 
   return (
-    <div className='z-10 flex items-center justify-center md:mx-10 lg:mx-10'>
-      {ThemeModes.map((mode, i) => (
-        <IconButton
-          key={i}
-          defaultIcon={mode.Icon}
-          classname={theme === mode.Mode ? 'dark:bg-black rounded-xl ring-1 ring-white/10 bg-zinc-950' : ''}
-          onClickHandle={() => handleModeChange(mode.Mode)}
-        />
-      ))}
+    <div className={`grid place-content-center transition-colors md:mx-10 lg:mx-10`}>
+      <div className='relative flex items-center'>
+        {ThemeModes.map((mode, i) => (
+          <button
+            key={i}
+            className='relative z-10 flex items-center gap-2 px-2 py-2 transition-colors'
+            onClick={() => handleModeChange(mode.Mode)}
+          >
+            {mode.Icon}
+          </button>
+        ))}
+
+        <div
+          className={`absolute inset-0 z-0 flex ${
+            theme === 'dark' ? 'justify-end' : theme === 'system' ? 'justify-center' : 'justify-start'
+          }`}
+        >
+          <motion.span
+            layout
+            transition={{ type: 'spring', damping: 15, stiffness: 250 }}
+            className='h-full w-1/3 rounded-xl border border-white/[0.2] bg-zinc-950 ring-1 ring-white/10'
+          />
+        </div>
+      </div>
     </div>
   );
 };
