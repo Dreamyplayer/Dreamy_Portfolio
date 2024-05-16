e1f731bc972eb59f8ab4ce4e19daae435a8655b5
Dreamyplayer <saikumarchinna123@gmai.com>
2024-05-16 22:35:50 +0530

    Implement initial page loading indicator [Feature]

--- src/app/themeProviders.tsx
+++ src/app/themeProviders.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+
+import Loading from '@/app/loading';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: { children: React.ReactNode } & ThemeProviderProps) {
-  return (
-    <>
-      <NextThemesProvider {...props}>{children}</NextThemesProvider>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  const intervalId = setInterval(() => {
+    if (typeof window !== 'undefined') {
+      const loadingState = window.document.readyState;
+
+      if (loadingState === 'complete') {
+        setIsLoading(false);
+        clearInterval(intervalId);
+      }
+    }
+  }, 100);
+
+  return <>{isLoading ? <Loading /> : <NextThemesProvider {...props}>{children}</NextThemesProvider>}</>;
 }
